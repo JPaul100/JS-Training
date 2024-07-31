d3af28f3a2590ed3501a100d0f65ef0ef624e9ea
JPaul100 <48398916+JPaul100@users.noreply.github.com>
2024-07-31 22:11:57 +0000

    Datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";// treat all JS code as newer version 
+
+// alert( 3 + 3) // we are using node JS , not browser 
+
+console.log( 3
+    + 
+    3) // code readibility should be high, not like this 
+
+console.log("Jeet");
+
+let name = "Jeet" ;
+let age = 18
+let isloggedin = false
+let state;
+
+// number => 2 to power of 53
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object 
+
+console.log(typeof "Jeet");
+
+
